6e132504a72414f837d347723605ba9376a83c02
MR.51MB4H <48198896+kingcracker@users.noreply.github.com>
2019-04-10 11:38:50 +0700

    Add files via upload

---
+++ hack.sh
@@ -0,0 +1,76 @@
+# ! /usr/bin/bash
+#tools sederhana
+#prank hack iklan tv
+#by MR.51MB4H
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+echo " "
+echo  $blue "<!÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷!>"
+echo  $red    "|       TOOLS HACK IKLAN TV               |"
+echo  $red    "|                                         |"
+echo  $red    "| Author        : Mr.51mb4h               |"
+echo  $red    "| date          : 11-4-2019               |"
+echo  $red    "| My Team       : Indenpendent Cyber Team |"
+echo  $red    "| selamat mencoba njiing                  |"
+echo  $blue "<!÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷!>"
+sleep 1
+echo " "
+echo $green "============== $red MENU $green ================="
+echo $blue "===================================="
+echo $blue "|| $green 1. Hapus Iklan Cara Halus      $blue ||"
+echo $blue "|| $green 2. Hapus Iklan Cara Biasa      $blue ||"
+echo $blue "|| $green 3. Hapus Iklan Cara Kasar      $blue ||"
+echo $blue "|| $green 4. Hapus Iklan Cara Gila       $blue ||"
+echo $blue "|| $green 0. Exit The Program            $blue ||"
+echo $blue "===================================="
+echo $blue"╭──[Masukkan Pilihan Anda]>"
+read -p"╰───────fight@KING CRACKER=•>" King ;
+if [ $King = 1 ] || [ $King = 01 ]
+then
+echo $blue"1.hidupkan tv"
+echo $green"2.cari yang ada iklan"
+echo $blue"3.pilih tombol power"
+echo $blue"4.sudah selesai"
+fi
+
+
+if [ $King = 2 ] || [ $King = 02 ]
+then
+echo $blue"1.hidupkan tv"
+echo $green"2.cari yang ada iklan"
+echo $blue"3.ganti channel yang gak iklan"
+echo $blue"4.sudah selesai..."
+fi
+
+if [ $King = 3 ] || [ $King = 03 ]
+then
+echo $blue"1.hidupkan tv"
+echo $green"2.cari yang ada iklan"
+echo $blue"3.ambil palu"
+echo $blue"4.pecahkan layar tv"
+echo $blue"5.sudah selesai"
+fi
+
+
+if [ $King = 4 ] || [ $King = 04 ]
+then
+echo $blue"1.hidupkan tv"
+echo $green"2.cari yang ada iklan"
+echo $blue"3.cabut stopkontak tv"
+echo $blue"4.angkat tv"
+echo $blue"5.banting tv"
+echo $blue"selesai iklan dah hilang"
+fi
+
+
+if [ $King = 0 ] || [ $King = 00 ]
+then
+echo  $red "dah dah.."
+echo  $blue "thanks sudah gunakan tools ini"
+echo  $green "Indenpendent Cyber Team!!"
+exit
+fi
